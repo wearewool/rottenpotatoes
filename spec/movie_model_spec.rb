46c2364d2642a5222f40774e15a3a215680a459f
tz2498 Tianjiao Zhu <tz2498@columbia.edu>
2022-10-27 02:56:54 +0000

    more test

--- spec/movie_model_spec.rb
+++ spec/movie_model_spec.rb
@@ -1,29 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Movie do
-  describe "call with_director" do
+  describe "call with_director function" do
     it "returns movies with the same director" do
       Movie.delete_all
-      movie1 = { :title => "movie with director 1", :rating => "R", :director => "director1" }
-      movie2 = { :title => "movie with director 2", :rating => "PG", :director => "director1" }
+      movie1 = { :title => "movie with director A", :rating => "R", :director => "directorA" }
+      movie2 = { :title => "movie with director B", :rating => "PG", :director => "directorA" }
       Movie.create(movie1)
       Movie.create(movie2)
 
-      result = Movie.with_director("director1")
-      expect(result.find { |movie| movie[:title] == "movie with director 1" }).to_not be_nil
-      expect(result.find { |movie| movie[:title] == "movie with director 2" }).to_not be_nil
+      result = Movie.with_director("directorA")
+      expect(result.find { |movie| movie[:title] == "movie with director A" }).to_not be_nil
+      expect(result.find { |movie| movie[:title] == "movie with director B" }).to_not be_nil
     end
 
     it "not return movies by different directors" do
       Movie.delete_all
-      movie1 = { :title => "movie with director 1", :rating => "R", :director => "director1" }
-      movie2 = { :title => "movie with director 2", :rating => "PG", :director => "director2" }
+      movie1 = { :title => "movie with director A", :rating => "R", :director => "directorA" }
+      movie2 = { :title => "movie with director B", :rating => "PG", :director => "directorB" }
       Movie.create(movie1)
       Movie.create(movie2)
 
-      result = Movie.with_director("director1")
-      expect(result.find { |movie| movie[:title] == "movie with director 1" }).to_not be_nil
-      expect(result.find { |movie| movie[:title] == "movie with director 2" }).to be_nil
+      result = Movie.with_director("directorA")
+      expect(result.find { |movie| movie[:title] == "movie with director A" }).to_not be_nil
+      expect(result.find { |movie| movie[:title] == "movie with director B" }).to be_nil
     end
   end
 end
